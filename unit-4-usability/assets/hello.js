a5380938afce81de7eeb79db48244506e41bfece
Zach Whalen <zach.whalen@gmail.com>
2026-04-02 10:46:33 -0400

    more hello world

--- unit-4-usability/assets/hello.js
+++ unit-4-usability/assets/hello.js
@@ -1,4 +1,16 @@
-console.log("Hello");
+
+let myForm = document.querySelector("#myForm");
+
+myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	// console.log("Hello");
+
+	console.log(this.msg.value)
+
+});
+
+
 
 
 
